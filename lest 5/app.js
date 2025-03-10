9bb675bf9310c54d23dd2ef61ee2ab185da62f5a
Superman <skamedulrahaman786@gmail.com>
2025-03-10 23:14:25 +0530

    Merge complete with all changes

--- lest 5/app.js
+++ lest 5/app.js
@@ -6,3 +6,27 @@ dotenv.config({path: "./.env"});
 const app = express();
 app.use(express.json());
 app.use("/user", userRouter);
+//product
+//order
+
+app.all("*", (req, res) => {
+    req.statusCode(400).json({
+        status: "fail",
+        massage: `Can't find ${req.originalUrl} on this server!`,
+    });
+});
+
+// GLOBAL ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.massage || "Internal Server Error";
+    err.status = err.status || "error";
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    });
+    return;
+});
+
+module.exports = app;
